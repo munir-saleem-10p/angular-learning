1bd1abd0d10d5fcb5dfec910f9bb78ce10066fa1
munir-saleem-10p <munir.saleem@10pearls.com>
2023-05-08 19:46:49 +0500

    learning about data and property bindings

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./server.component.scss']
 })
 export class ServerComponent implements OnInit {
+  serverId = 100;
+  serverStatus = 'offline';
 
   constructor() { }
 
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,13 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  selector: 'app-servers', //default. usage <app-server></app-server>
+
+  // selector: '[app-servers]', // by applying square brackets we are using selector as attribute on HTML elements like div.
+
+  // selector: '.app-servers', // we can also use it with .(dot) and can also use as css selector. usage <div class="app-servers"></div>. Info by id is not supported by angular.
+
+
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.scss']
 })
 export class ServersComponent implements OnInit {
+  serverId= 10;
+  addNewServer: boolean = false;
 
-  constructor() { }
+  constructor(){
+    setTimeout(() => {
+      this.addNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {
   }
